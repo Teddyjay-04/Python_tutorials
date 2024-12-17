9569d6efad3c1c410e3d36b2887db8a6eeb7e206
Teddyjay-04 <jamesteddy411@gmail.com>
2024-12-17 23:41:50 +0000

    Syntax handling in python

---
+++ Week_1/syntax.py
@@ -0,0 +1,13 @@
+python_indentation = "refers to the spaces at the beginning of a code line."
+# Python uses indentation to indicate a block of code.
+if 5 > 2:
+    print("Five is greater than two!")
+
+"""Python will give you an error if you skip the indentation:
+
+
+Syntax Error:
+
+if 5 > 2:
+print("Five is greater than two!")
+"""
