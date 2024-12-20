b58ec17473c61830a3649d0a40f4970e54ddb3e8
Teddyjay-04 <jamesteddy411@gmail.com>
2024-12-20 23:40:57 +0000

    Initial commit: Add data types example

--- Week_1/data_types.py
+++ Week_1/data_types.py
@@ -9,6 +9,8 @@ Python has the following data types built-in by default, in these categories:'''
 print(type('Hello, World!'))  # Output: <class'str'>
 x = 5
 print(type(x))# Output: <class'int'>
+
+
 # type: ignore
 
 #Numeric Types:	int, float, complex
@@ -18,3 +20,20 @@ print(type(x))# Output: <class'int'>
 #Boolean Type:	bool
 #Binary Types:	bytes, bytearray, memoryview
 #None Type:	NoneType
+
+"""Example	Data Type
+x = "Hello World"	str	
+x = 20	int	
+x = 20.5	float	
+x = 1j	complex	
+x = ["apple", "banana", "cherry"]	list	
+x = ("apple", "banana", "cherry")	tuple	
+x = range(6)	range	
+x = {"name" : "John", "age" : 36}	dict	
+x = {"apple", "banana", "cherry"}	set	
+x = frozenset({"apple", "banana", "cherry"})	frozenset	
+x = True	bool	
+x = b"Hello"	bytes	
+x = bytearray(5)	bytearray	
+x = memoryview(bytes(5))	memoryview	
+x = None	NoneType"""
